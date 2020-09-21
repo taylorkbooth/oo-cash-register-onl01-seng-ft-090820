2ab76015f8441daa0ad14d720b9a8cb3e7d3f4c1
Taylor Booth <taylorkbooth@gmail.com>
2020-09-21 00:04:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,6 +9,22 @@ class CashRegister
   end
   
   
+  def add_item
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+  
   
   
   
