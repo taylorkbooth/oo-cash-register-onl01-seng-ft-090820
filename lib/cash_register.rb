d3eaa167f8738e24ab30cc5a606357976cc05ffe
Taylor Booth <taylorkbooth@gmail.com>
2020-09-21 00:37:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   
   attr_accessor :total, :discount
   
-  def initialize(discount = 20)
+  def initialize(discount = 0)
     @total = 0
     @discount = discount
   #   @items = []
