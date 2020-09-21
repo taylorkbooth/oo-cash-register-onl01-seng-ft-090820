b87a59fe6ef8638819c7b5d1bfab8ff62d7364ac
Taylor Booth <taylorkbooth@gmail.com>
2020-09-21 00:38:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,9 +12,8 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     quantity.times do
       @items << title
-      
-      
   end
+  last item
   
   def apply_discount
     @total = @total * @discount / 100
